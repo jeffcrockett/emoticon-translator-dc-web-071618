abaaacb81c9eff357a345a7cd29e9c41b2a9e9f0
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-07-10 23:04:08 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
   "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   hash = load_library(file_path)
   hash.each do |key, value_hash|
     if key == "get_emoticon"
